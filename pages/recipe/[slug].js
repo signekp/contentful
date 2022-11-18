e5471e7928c0b81185bdd7bf0b7d1cc7d6629131
signekp <sign4080@gmail.com>
2022-11-18 11:53:56 +0100

    fallback

--- pages/recipe/[slug].js
+++ pages/recipe/[slug].js
@@ -1,6 +1,7 @@
 import { createClient } from "contentful";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import Image from "next/image";
+import Skeleton from "../../components/Skeleton";
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -20,7 +21,7 @@ export const getStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false,
+    fallback: true,
   };
 };
 
@@ -37,6 +38,7 @@ export const getStaticProps = async ({ params }) => {
 };
 
 export default function RecipeDetails({ recipe }) {
+  if (!recipe) return <Skeleton />;
   const { featuredImage, title, cookingTime, ingredients, method } =
     recipe.fields;
   return (
